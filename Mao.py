585bbc3c46fa76fe95f4082b753e021bafeaa4d2
Patricia Raposo Santana Lima <36173425+Patricia-RSL@users.noreply.github.com>
2018-09-19 20:39:39 -0300

    criação do metodo mao_most_valorosa SEM TESTES

--- Mao.py
+++ Mao.py
@@ -24,4 +24,8 @@ class Mao:
 				if x.get_soma() > response.get_soma():
 					response = x
 		return response
-
+	def mao_most_valorosa(maoA, maoB):## retorna True se maoA é quem começa
+		fakeMao = Mao([maoA.most_valorosa(), maoB.most_valorosa()])
+		if Pedra.is_igual(fakeMao.most_valorosa(),maoA,most_valorosa()):
+			return True
+		return False
